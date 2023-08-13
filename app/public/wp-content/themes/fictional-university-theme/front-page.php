b4c219c2c9cb6d57eb25341aee372298170a648c
persis-randolph <persis.randolph@gmail.com>
2023-08-13 17:54:37 -0500

    add custom post type (events) - 27

--- app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -25,7 +25,7 @@
             Upcoming Events
           </h2>
 
-          <div class="event-summary">
+          <!-- <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
               <span class="event-summary__month">Mar</span>
               <span class="event-summary__day">25</span>
@@ -40,22 +40,7 @@
                 <a href="#" class="nu gray">Learn more</a>
               </p>
             </div>
-          </div>
-          <div class="event-summary">
-            <a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month">Apr</span>
-              <span class="event-summary__day">02</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny">
-                <a href="#">Quad Picnic Party</a>
-              </h5>
-              <p>
-                Live music, a taco truck and more can found in our third annual
-                quad picnic day. <a href="#" class="nu gray">Learn more</a>
-              </p>
-            </div>
-          </div>
+          </div> -->
 
           <p class="t-center no-margin">
             <a href="#" class="btn btn--blue">View All Events</a>
